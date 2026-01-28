af41a7e1d4fb5326db9bda522267c51603ec078d
itoshusu <sopfantasy@yahoo.co.jp>
2026-01-29 01:02:17 +0900

    locales/views/ja.yml撲滅

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,9 +15,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       start_new_session_for(@user)
-      redirect_to after_authentication_url, notice: t('.success')
+      redirect_to after_authentication_url, notice: 'ユーザー登録が完了しました'
     else
-      flash.now[:danger] = t('.fail')
+      flash.now[:danger] = 'ユーザー登録に失敗しました'
       render 'new', status: :unprocessable_content
     end
   end
